034e285b98570ae392a7466447603c2fb2a37467
Abdullah149081 <masudmd425@gmail.com>
2023-02-16 19:27:47 +0600

    calculator add Delegate Method

--- js/pinGenerator.js
+++ js/pinGenerator.js
@@ -19,3 +19,22 @@ document.getElementById("pin-generate").addEventListener("click", function () {
   const pinInput = document.getElementById("pin-input");
   pinInput.value = pinGetValue;
 });
+
+document.getElementById("calculator").addEventListener("click", function (event) {
+  const clickButton = event.target.innerText;
+  const calInputField = document.getElementById("cal-input");
+  const addNewNumber = calInputField.value;
+  if (isNaN(clickButton)) {
+    if (clickButton === "C") {
+      calInputField.value = "";
+    } else if (clickButton === "<") {
+      const deleteLastValue = addNewNumber.split("");
+      deleteLastValue.pop();
+      const joinDigit = deleteLastValue.join("");
+      calInputField.value = joinDigit;
+    }
+  } else {
+    const sumAdd = addNewNumber + clickButton;
+    calInputField.value = sumAdd;
+  }
+});
